ee1d7e4a24355028374f66fe6c3f4771391ad6ea
Naru <tristan.etude@gmail.com>
2024-05-31 04:42:22 +0200

    Url RS + Présentation
    Ajout des Url RS
    Modification de la présentation

--- mon-portfolio/src/composants/Aboutme/AboutMe.jsx
+++ mon-portfolio/src/composants/Aboutme/AboutMe.jsx
@@ -7,15 +7,18 @@ const AboutMe = () => {
       <div className="about-me-text">
         <h2>À propos de moi</h2>
         <p>
-          Bonjour! Je suis [Votre Nom], un développeur web passionné par la création de sites et d'applications web interactives et intuitives. Avec plusieurs années d'expérience dans le domaine, j'ai travaillé sur divers projets qui m'ont permis d'affiner mes compétences et d'acquérir une expertise approfondie.
+          Bonjour! Je suis Tristan, un développeur agé de 25ans en plein apprentissage mais avec beaucoup d'ambition. 
+          Ayant commencé à me former depuis l'an dernier via OpenClassroom en tant qu'intégrateur web. 
+          J'ai pu apprendre à travailler en autonomie et à devoir remettre en quesiton mon travail pour le parfaire. 
+          Ce qui m'offre une bonne capacité d'analyse dans ce domaine et des bases solides en dévellopement.
         </p>
       </div>
       <div className="about-me-image">
         <img src="[URL de votre image]" alt="Votre Nom" />
         <div className="social-links">
-          <a href="https://twitter.com/votreprofil" target="_blank" rel="noopener noreferrer">Twitter</a>
-          <a href="https://linkedin.com/in/votreprofil" target="_blank" rel="noopener noreferrer">LinkedIn</a>
-          <a href="https://github.com/votreprofil" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a href="https://twitter.com/NaruLive_" target="_blank" rel="noopener noreferrer">Twitter</a>
+          <a href="https://www.linkedin.com/in/tristan-b-385235266/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://github.com/TrisBer" target="_blank" rel="noopener noreferrer">GitHub</a>
         </div>
       </div>
     </section>
